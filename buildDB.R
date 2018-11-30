2526032dd880e8ea1c0a133c82c478d1cb490796
Sam Carlson <sam.p.carlson@gmail.com>
2018-11-30 12:28:16 -0600

    fix recursive flags definition bug - update defaults at top level function call with ...

--- buildDB.R
+++ buildDB.R
@@ -1,14 +1,17 @@
 library(RSQLite)
+library(tidyverse)
 source("dbTools.R")
 source("C:/Users/sam/Documents/R/projects/rGrassTools/grassTools.r")
 
 leakyDB=dbConnect(SQLite(),"C:/Users/sam/Documents/LeakyRivers/Data/sqLiteDatabase/LeakyDB.db")
-dbShapePath="C:/Users/sam/Documents/LeakyRivers/Data/sqLiteDatabase/shapes/"
-targetEPSG=32613
-streamSnapDistCells=20
-#currently relying on the threshold set in r.watershed
-#streamSnapThresholdCells=5000
-addMidpoint=T
+#set all defaults
+defaultFlags=list(inEPSG=32613,
+           dbEPSG=32613,
+           streamSnapDistCells=20,
+           onStream=T,
+           addMidpoint=T,
+           dbShapePath="C:/Users/sam/Documents/LeakyRivers/Data/sqLiteDatabase/shapes/",
+           dbShapeName="leakyArea_")
 
 dbListTables(leakyDB)
 
@@ -20,11 +23,11 @@ lapply(dbListTables(leakyDB),FUN=delete_data,db=leakyDB)
 InitGrass_byRaster()
 
 ############------------define watersheds----------###########
-wshedDefs=addWatershedDefinitions( read.csv('C:/Users/sam/Documents/spatial/data/WatershedOutflowPoints/allWsheds_13n.csv') )
+wshedDefs=addWatershedDefinitions( wshedDefs=read.csv('C:/Users/sam/Documents/spatial/data/WatershedOutflowPoints/allWsheds_13n.csv'),
+                                   addMidpoint=F, dbShapeName="watershedArea_")
 
 
 ###################-------------------add widths (from Mike and I) --------################
 widths=read.csv("C:/Users/Sam/Documents/LeakyRivers/Data/width/FinalWidths_11_2018.csv")
 #can't handle data without coordinates - bother mike about this if necessary
 widths=widths[complete.cases(widths[,c("X","Y")]),]
-addData( inEPSG=4326)
--- dbTools.R
+++ dbTools.R
@@ -2,49 +2,72 @@ library(rgrass7)
 library(sp)
 library(rgdal)
 
+updateFlags=function(flags,...){
+  l = list(...)
+  for(n in names(l)){
+    if(n %in% names(flags)){
+      flags[names(flags)==n]=l[names(l)==n]
+    } else print(paste("Unknown flag:",n))
+  }
+  return(flags)
+}
+
 delete_data=function(db,table){
   dbExecute(db,paste0("DELETE FROM ",table))
 }
 
-addData=function(dataDF,                                      #data
-                 batchName,batchSource,                       #batch
-                 metric,unit,method,                          #data type
-                 pointXYdf=NULL,inShpDSN=NULL, areaName=NULL, #location
-                 inEPSG=targetEPSG,snapToStream=T,targetEPSG=targetEPSG, addMidpoint=addMidpoint){ #defaults
-  dataTypeIDX=addDataType(metric,unit,method)
-  batchIDX=addBatch(batchName,batchSource)
-  locationIDX
+#data, containing $X, $Y, $dateTime, $value, $QCStatusOK, $metric, $unit, $method
+addData_points=function(dataDF,batchName,batchSource,flags=defaultFlags,...){
+  f=updateFlags(flags,...)
+  if(!"QCStatusOK" %in% names(dataDF)){
+    dataDF$QCStatusOK="TRUE"
+  }
+  addData_points_worker=function(dataDF, #single row
+                                 batchName,batchSource){
+    dataTypeIDX=addDataType(metric,unit,method)
+    batchIDX=addBatch(batchName,batchSource)
+    locationIDX=addLocation(pointXYdf=dataDF,flags=f)
+    
+    writeDF=data.frame(dataTypeIDX=dataTypeIDX,batchIDX=batchIDX,locationIDX=locationIDX,
+                       dateTime=dataDF$dateTime, value=dataDF$value, QCStatusOK = data$QCStatusOK)
+    dataIDX=writeIfNew(writeDF = writeDF, table="data",
+                       compareNames=c("dataTypeIDX","batchIDX","locationIDX","dateTime","value","QCStatusOK"),
+                       idxColName = "dataIDX")
+  }
   
+  apply(X=dataDF,MARGIN = 1, FUN=addData_points_worker)
 }
 
-addWatershedDefinitions=function(wshedDefs){
+addWatershedDefinitions=function(wshedDefs,flags=defaultFlags,...){
+  f=updateFlags(flags,...)
   #InitGrass_byRaster(rasterPath="C:/Users/Sam/Documents/spatial/r_workspaces/LeakyDB/streamsRast_xxl.tif",grassRasterName="streamRast")
   addRasterIfAbsent(grassName="streamRast",rasterPath="C:/Users/Sam/Documents/spatial/r_workspaces/LeakyDB/streamsRast_xxl.tif")
-  execGRASS("r.in.gdal")
   
-  writeVECT(SDF=SpatialPointsDataFrame(coords=wshedDefs[,c("X","Y")],data=wshedDefs[,c("WshedName","id")]),"wshedOutPoints",v.in.ogr_flags=c("o","quiet"))
-  execGRASS("r.stream.snap",input="wshedOutPoints",stream_rast="streamRast",radius=10,output="wshedOutPoints_snap",flags="quiet")
+  writeVECT(SDF=SpatialPointsDataFrame(coords=wshedDefs[,c("X","Y")],data=wshedDefs[,c("WshedName","id")]),"wshedOutPoints",v.in.ogr_flags=c("overwrite","o","quiet"))
+  execGRASS("r.stream.snap",input="wshedOutPoints",stream_rast="streamRast",radius=10,output="wshedOutPoints_snap",flags=c("overwrite","quiet"))
   execGRASS("v.db.addtable",map="wshedOutPoints_snap",flags="quiet")
-  execGRASS("v.db.join",map="wshedOutPoints_snap",column="cat",other_table="wshedOutPoints",other_column="cat",flags="quiet")
+  execGRASS("v.db.join",map="wshedOutPoints_snap",column="cat",other_table="wshedOutPoints",other_column="cat",flags=c("quiet"))
   wshedDefs=grassTableToDF( execGRASS("v.report",map="wshedOutPoints_snap",option="coor",flags="quiet", intern=T) )
-  execGRASS("r.in.gdal",input="C:/Users/Sam/Documents/spatial/r_workspaces/LeakyDB/flowDir_xxl.tif",output="flowDir",flags="quiet")
+  addRasterIfAbsent(grassName="flowDir",rasterPath="C:/Users/Sam/Documents/spatial/r_workspaces/LeakyDB/flowDir_xxl.tif")
   
   for(i in 1:nrow(wshedDefs)){
+    thisName=wshedDefs$WshedName[i]
     #calculate and write area:
-    execGRASS("r.water.outlet", input="flowDir",output="above_point_temp",coordinates=c(wshedDefs$x[i],wshedDefs$y[i]), flags=c("overwrite","quiet"))
+    execGRASS("r.water.outlet", input="flowDir",output="above_point_temp",coordinates=c(as.numeric(wshedDefs$x[i]),as.numeric(wshedDefs$y[i])), flags=c("overwrite","quiet"))
     execGRASS("r.to.vect",input="above_point_temp",output="dbWriteMe",type="area",flags=c("overwrite","quiet"))
-    execGRASS("v.out.ogr",input="dbWriteMe",output=paste0(dbShapePath,"tempShape.shp"),format="ESRI_Shapefile",flags=c("overwrite","quiet"))
-    
-    thisAreaIDX=addArea(EPSG=32613)
+    execGRASS("v.out.ogr",input="dbWriteMe",output=paste0(f$dbShapePath,"tempShape.shp"),format="ESRI_Shapefile",flags=c("overwrite","quiet"))
+    thisAreaIDX=addArea(areaName=thisName,inShpDSN=paste0(f$dbShapePath,"tempShape.shp"),flags=f)
     
     #calculate and write point:
-    thisPointIDX=addPoint(X=wshedDefs$x[i],Y=wshedDefs$y[i])
+    thisPointIDX=addPoint(X=wshedDefs$x[i],Y=wshedDefs$y[i],flags=f)
     
     #write to watersheds table:
     #writeIfNew(writeDF=writeDF,table="Watersheds",compareNames = c("areaIDX","outPointIDX"),idxColName = "watershedID")
     #ignore duplicate issues here by writing exactly once - these are single-purpose points
     
-    dbWriteTable(leakyDB,"Watersheds",data.frame(watershedID=wshedDefs$WshedName[i],areaIDX=thisAreaIDX,outPointIDX=thisPointIDX),append=T)
+    writeDF=data.frame(watershedID=wshedDefs$WshedName[i],areaIDX=thisAreaIDX,outPointIDX=thisPointIDX)
+    
+    dbWriteTable(leakyDB,name="Watersheds",value=writeDF,append=T)
   }
   return(wshedDefs$WshedName)
 }
@@ -68,10 +91,17 @@ writeIfNew=function(writeDF,table,compareNames,idxColName,write=T){    #works wi
     thisVal=df[names(df)==name][1,1]
     return(paste0(name,"='",thisVal,"'"))
   }
+  whereDF=writeDF[,names(writeDF) %in% compareNames]
   
-  where=paste0(sapply(names(writeDF),FUN=returnNameVal,df=writeDF),collapse=" AND ")
+  if(length(compareNames)>1){
+    where=paste0(sapply(names(whereDF),FUN=returnNameVal,df=whereDF),collapse=" AND ")
+    dataTypeIDX=dbGetQuery(leakyDB,paste0("SELECT ",idxColName," FROM ",table," WHERE ",where))[[1]]
+  } 
   
-  dataTypeIDX=dbGetQuery(leakyDB,paste0("SELECT ",idxColName," FROM ",table," WHERE ",where))[[1]]
+  if(length(compareNames)==1){
+    dataTypeIDX=dbGetQuery(leakyDB,paste0("SELECT ",idxColName," FROM ",table," WHERE ",compareNames,"='",whereDF[[1]],"'"))[[1]]
+  }
+  
   if(length(dataTypeIDX)==0){
     newIDX=getNewIDX(table=table,idxColName=idxColName)
     writeDF$tempIDX=newIDX
@@ -99,10 +129,11 @@ addBatch=function(batchName,batchSource){
   return(batchIDX)
 }
 
-addLocation=function(pointXYdf=NULL,inShpDSN=NULL,areaName=NULL,inEPSG=targetEPSG,snapToStream=T,targetEPSG=targetEPSG, addMidpoint = addMidpoint){
+addLocation=function(pointXYdf=NULL,inShpDSN=NULL,areaName=NULL,flags){
+  f=flags
   if(!is.null(areaName)){
-    areaIDXs=addArea(areaName,inShpDSN,inEPSG=targetEPSG,targetEPSG=targetEPSG,dbShapePath=dbShapePath,dbShapeName="leakyArea_",addMidpoint = addMidpoint)
-    if(addMidpoint){
+    areaIDXs=addArea(areaName,inShpDSN,flags=f)
+    if(f$addMidpoint){
       areaIDX=areaIDXs$areaIDX
       pointIDX=areaIDXs$pointIDX
     } else {
@@ -110,7 +141,7 @@ addLocation=function(pointXYdf=NULL,inShpDSN=NULL,areaName=NULL,inEPSG=targetEPS
     }
   } else {
     areaIDX=NULL
-    pointIDX=addPoint(X=pointXYdf$X,Y=pointXYdf$Y,inEPSG=inEPSG,snapToStream = snapToStream, targetEPSG = targetEPSG)
+    pointIDX=addPoint(X=pointXYdf$X,Y=pointXYdf$Y,flags=f)
   }
   #areas have midpoints by default
   writeDF=data.frame(isPoint=T,pointIDX=pointIDX,areaIDX=NULL,watershedID="not assigned")
@@ -120,49 +151,56 @@ addLocation=function(pointXYdf=NULL,inShpDSN=NULL,areaName=NULL,inEPSG=targetEPS
   return(locationIDX)
 }
 
-addPoint=function(X,Y,inEPSG=targetEPSG,snapToStream=T,targetEPSG=targetEPSG){
-  writeDF=data.frame(X=X,Y=Y,EPSG=EPSG,onStream=onStream)
+addPoint=function(X,Y,flags){
+  f=flags
+  writeDF=data.frame(X=X,Y=Y,EPSG=f$inEPSG,onStream=f$onStream)
   
-  if(snapToStream){
-    writeDF=snapToStream(writeDF, inEPSG = inEPSG, targetEPSG=targetEPSG)
-  } else {
-    writeDF=convertEPSG_df(writeDF,inEPSG=inEPSG, targetEPSG=targetEPSG)
+  if(f$inEPSG!=f$dbEPSG){
+    writeDF=convertEPSG_df(writeDF,inEPSG=f$inEPSG,flags=f)
+  }
+  
+  if(f$onStream){
+    writeDF=snapToStream(writeDF,flags=f)
   }
   
   pointIDX=writeIfNew(writeDF = writeDF, table = "Points",compareNames = c("X","Y","EPSG","onStream"),idxColName = "pointIDX")
   return(pointIDX)
 }
 
-convertEPSG_df=function(inXYdf, inEPSG=4326, targetEPSG=targetEPSG){
-  
+convertEPSG_df=function(inXYdf,flags){
+  f=flags
   sp::coordinates(inXYdf)=c("X","Y")
-  inp4s=paste0("+init=epsg:",inEPSG)
-  newp4s=paste0("+init=epsg:",targetEPSG)
+  inp4s=paste0("+init=epsg:",f$inEPSG)
+  newp4s=paste0("+init=epsg:",f$dbEPSG)
   
-  proj4string(projectMe)=inp4s
+  proj4string(inXYdf)=inp4s
   projected=spTransform(inXYdf,newp4s)
   result=cbind(projected@data,projected@coords)
   return(result)
 }
 
-convertEPSG_area=function(inShpDSN = inShpDSN,targetEPSG=targetEPSG){
+convertEPSG_area=function(inShpDSN,flags){
+  f=flags
   tempShape=rgdal::readOGR(dsn=inShpDSN)
-  newp4s=paste0("+init=epsg:",targetEPSG)
+  newp4s=paste0("+init=epsg:",f$dbEPSG)
   tempShape_projected=spTransform(tempShape,newp4s)
   rgdal::writeOGR(tempShape_projected,inShpDSN,driver="ESRI Shapefile",layer="area",overwrite_layer = TRUE)
 }
 
-snapToStream=function(inXYdf, inEPSG=targetEPSG, targetEPSG=targetEPSG){
-  if(inEPSG!=targetEPSG){
-    inXYdf=convertEPSG_df(inXYdf,inEPSG=inEPSG, targetEPSG=targetEPSG)
+snapToStream=function(inXYdf,flags){
+  f=flags
+  if(f$inEPSG!=f$dbEPSG){
+    inXYdf=convertEPSG_df(inXYdf,flags=f)
   }
   
   inXYdf$tempSnapID=1:nrow(inXYdf)
   
   #InitGrass_byRaster(rasterPath="C:/Users/Sam/Documents/spatial/r_workspaces/LeakyDB/streamsRast_xxl.tif",grassRasterName="streamRast")
   addRasterIfAbsent(grassName="streamRast",rasterPath="C:/Users/Sam/Documents/spatial/r_workspaces/LeakyDB/streamsRast_xxl.tif")
+  inXYdf$X=as.numeric(as.character(inXYdf$X))
+  inXYdf$Y=as.numeric(as.character(inXYdf$Y))
   writeVECT(SDF=SpatialPointsDataFrame(coords=inXYdf[,c("X","Y")],data=data.frame(tempSnapID=inXYdf$tempSnapID)),
-            "tempShape",v.in.ogr_flags = c("o","overwrite"))
+            "tempShape",v.in.ogr_flags = c("quiet","o","overwrite"))
   
   execGRASS("r.stream.snap",input="tempShape",stream_rast="streamRast",radius=20,output="tempShape_snap",flags=c("overwrite","quiet"))
   execGRASS("v.db.addtable",map="tempShape_snap",flags="quiet")
@@ -171,27 +209,28 @@ snapToStream=function(inXYdf, inEPSG=targetEPSG, targetEPSG=targetEPSG){
   result=left_join(snapped[,c("tempSnapID","x","y")],inXYdf[,!names(inXYdf) %in% c("X","Y")],by="tempSnapID")
   names(result)[names(result)=="x"]="X"
   names(result)[names(result)=="y"]="Y"
+  result=result[,names(result) != "tempSnapID"]
   return(result)
 }
 
-addArea=function(areaName, inShpDSN=inShpDSN,inEPSG=targetEPSG,targetEPSG=targetEPSG, dbShapePath=dbShapePath, dbShapeName=dbShapeName, addMidpoint=addMidpoint){
+addArea=function(areaName,inShpDSN,flags){
+  f=flags
   writeDF=data.frame(name=areaName)
   newAreaIDX=writeIfNew(writeDF = writeDF, table = "Areas" ,compareNames = c("name"),idxColName ="areaIDX",write=F) 
   
-  if(inEPSG!=targetEPSG){
-    convertEPSG_area( inShpDSN = inShpDSN, targetEPSG = targetEPSG)
+  if(!f$inEPSG==f$dbEPSG){
+    convertEPSG_area(inShpDSN,flags=f)
   }
   tempShape=rgdal::readOGR(dsn=inShpDSN)
   
-  shpName=paste0(dbShapeName,newAreaIDX,".shp")
-  fullPathName=paste0(dbShapePath,shpName)
-  writeDF=data.frame(areaIDX=newAreaIDX,name=areaName,fileName=fullPathName,EPSG=EPSG)
+  shpName=paste0(f$dbShapeName,newAreaIDX,".shp")
+  fullPathName=paste0(f$dbShapePath,shpName)
+  writeDF=data.frame(areaIDX=newAreaIDX,name=areaName,fileName=fullPathName,EPSG=f$dbEPSG)
   
   newAreaIDX=writeIfNew(writeDF = writeDF, table = "Areas" ,compareNames = c("name","fileName","EPSG"),idxColName ="areaIDX") 
   
   rgdal::writeOGR(tempShape,fullPathName,driver="ESRI Shapefile",layer="area",overwrite_layer = TRUE)
-  
-  if(addMidpoint){
+  if(f$addMidpoint){
     execGRASS("v.in.ogr", input=fullPathName,output="tempShape",flags=c("quiet","overwrite"))
     addRasterIfAbsent(grassName="streamRast",rasterPath="C:/Users/Sam/Documents/spatial/r_workspaces/LeakyDB/streamsRast_xxl.tif")
     #addRasterIfAbsent(grassName="UAA",rasterPath="C:/Users/Sam/Documents/spatial/r_workspaces/LeakyDB/flowAccum_xxl.tif")
@@ -203,7 +242,7 @@ addArea=function(areaName, inShpDSN=inShpDSN,inEPSG=targetEPSG,targetEPSG=target
     X=xy[[1]][1]
     Y=xy[[1]][2]
     
-    pointIDX=addPoint(X=X, Y=Y)
+    pointIDX=addPoint(X=X, Y=Y,flags=f)
     newIDXs=list(areaIDX=newAreaIDX,pointIDX=pointIDX)
     return( newIDXs )
   } else {
@@ -212,4 +251,4 @@ addArea=function(areaName, inShpDSN=inShpDSN,inEPSG=targetEPSG,targetEPSG=target
   
 }
 
-inWatershed=function(defPointIDX=NULL,defXY=NULL){}
+inWatershed=function(defPointIDX=NULL,defXY=NULL,flags,...){}
